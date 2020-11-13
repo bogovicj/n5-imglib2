ae99426a972bb6689534b35434837caaebf7210b
bogovicj <bogovicj@janelia.hhmi.org>
2020-11-13 11:25:42 -0500

    add conversion of general RealTransforms to dfields
    * and use optimized DeformationField implementation

--- src/main/java/org/janelia/saalfeldlab/n5/imglib2/N5DisplacementField.java
+++ src/main/java/org/janelia/saalfeldlab/n5/imglib2/N5DisplacementField.java
@@ -1,36 +1,44 @@
 package org.janelia.saalfeldlab.n5.imglib2;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.LongStream;
 
 import org.janelia.saalfeldlab.n5.Compression;
+import org.janelia.saalfeldlab.n5.DataType;
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.N5Writer;
 
 import net.imglib2.Cursor;
-import net.imglib2.FinalInterval;
 import net.imglib2.Interval;
 import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealPoint;
 import net.imglib2.RealRandomAccessible;
 import net.imglib2.converter.Converter;
 import net.imglib2.converter.Converters;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.cell.CellGrid;
 import net.imglib2.interpolation.InterpolatorFactory;
 import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
 import net.imglib2.realtransform.AffineGet;
 import net.imglib2.realtransform.AffineTransform;
-import net.imglib2.realtransform.AffineTransform2D;
-import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.realtransform.ExplicitInvertibleRealTransform;
 import net.imglib2.realtransform.DeformationFieldTransform;
-import net.imglib2.realtransform.InverseRealTransform;
 import net.imglib2.realtransform.InvertibleRealTransform;
-import net.imglib2.realtransform.InvertibleRealTransformSequence;
 import net.imglib2.realtransform.RealTransform;
-import net.imglib2.realtransform.RealTransformRandomAccessible;
 import net.imglib2.realtransform.RealTransformSequence;
 import net.imglib2.realtransform.RealViews;
+import net.imglib2.realtransform.Scale;
+import net.imglib2.realtransform.Scale2D;
+import net.imglib2.realtransform.Scale3D;
+import net.imglib2.realtransform.Translation;
 import net.imglib2.transform.integer.MixedTransform;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.IntegerType;
@@ -45,10 +53,13 @@ import net.imglib2.type.numeric.integer.UnsignedLongType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Intervals;
 import net.imglib2.util.Util;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.MixedTransformView;
 import net.imglib2.view.Views;
+import net.imglib2.view.composite.CompositeIntervalView;
+import net.imglib2.view.composite.GenericComposite;
 
 /**
  * Class with helper methods for saving displacement field transformations as N5 datasets.
@@ -67,6 +78,9 @@ public class N5DisplacementField
 	public static final String EXTEND_ZERO = "ext_zero";
 	public static final String EXTEND_MIRROR = "ext_mirror";
 	public static final String EXTEND_BORDER = "ext_border";
+
+	public final static int[] PERMUTATION2D = new int[] { 2, 0, 1 };
+	public final static int[] PERMUTATION3D = new int[] { 3, 0, 1, 2 };
 
 
     /**
@@ -93,6 +107,279 @@ public class N5DisplacementField
 	{
 	    save( n5Writer, FORWARD_ATTR, affine, forwardDfield, fwdspacing, blockSize, compression );
 	    save( n5Writer, INVERSE_ATTR, affine.inverse(), inverseDfield, invspacing, blockSize, compression );
+	}
+
+	public static < Q extends IntegerType< Q > > Converter< DoubleType, Q > quantizationConverter(
+			final int nd, final double maxError, final Q type )
+	{
+		/* 
+		 * To keep the max vector error below maxError, 
+		 * the error per coordinate must be below m
+		 */
+		final double m = 2 * Math.sqrt( maxError * maxError / nd );
+		return new Converter< DoubleType, Q >()
+		{
+			@Override
+			public void convert( DoubleType input, Q output )
+			{
+				output.setInteger( Math.round( input.getRealDouble() / m ) );
+			}
+		};
+	}
+
+	public static final <T extends NativeType<T> > void save(
+			final N5Writer n5Writer,
+			final String dataset,
+			final int[] spatialBlockSize,
+			final Compression compression,
+			final RealTransform transform,
+			final AffineGet pixelToPhysical,
+			final Interval interval,
+			final T quantizationType,
+			final double maxError,
+			ExecutorService threadPool ) throws IOException
+	{
+		final int ndims = transform.numSourceDimensions();
+
+		int[] permutation;
+		if( ndims == 2 )
+			permutation = PERMUTATION2D;
+		else
+			permutation = PERMUTATION3D;
+
+		final long[] spatialDimensions = interval.dimensionsAsLongArray();
+
+		final long[] outputDimensions = new long[ ndims + 1];
+		final int[] blockSize = new int[ ndims + 1];
+		outputDimensions[ 0 ] = ndims;
+		blockSize[ 0 ] = ndims;
+		for( int i = 0; i < ndims; i++ )
+		{
+			outputDimensions[ i + 1 ] = spatialDimensions[ i ];
+			blockSize[ i + 1 ] = spatialBlockSize[ i ];
+		}
+
+		final CellGrid outputCellGrid = new CellGrid( outputDimensions, blockSize );
+		final long numBlocks = Intervals.numElements( outputCellGrid.getGridDimensions() );
+		final List< Long > blockIndexes = LongStream.range( 0, numBlocks ).boxed().collect( Collectors.toList() );
+		System.out.println( "Num blocks: " + numBlocks );
+
+		ArrayImgFactory< T > blockFactory = new ArrayImgFactory<>( quantizationType );
+
+		final DataType datatype;
+		final boolean isQuantized;
+		if( quantizationType instanceof ByteType )
+		{
+			datatype = DataType.INT8;
+			isQuantized = true;
+		}
+		else if( quantizationType instanceof ShortType )
+		{
+			datatype = DataType.INT16;
+			isQuantized = true;
+		}
+		else if( quantizationType instanceof IntType )
+		{
+			datatype = DataType.INT32;
+			isQuantized = true;
+		}
+		else if( quantizationType instanceof FloatType )
+		{
+			datatype = DataType.FLOAT32;
+			isQuantized = false;
+		}
+		else if( quantizationType instanceof DoubleType )
+		{
+			datatype = DataType.FLOAT64;
+			isQuantized = false;
+		}
+		else
+		{
+			System.err.println( "Type not yet supported" );
+			return;
+		}
+
+		try
+		{
+			System.out.println( "Create dataset" );
+			n5Writer.createDataset( dataset,
+					new DatasetAttributes( outputDimensions, blockSize, datatype, compression ));
+		}
+		catch ( IOException e1 )
+		{
+			System.err.println( "Could not create dataset" );
+			e1.printStackTrace();
+			return;
+		}
+
+		for( final long i : blockIndexes )
+		{
+			System.out.println( "submit " + i);
+			threadPool.submit( new Runnable()
+			{
+				@SuppressWarnings( "unchecked" )
+				@Override
+				public void run()
+				{
+					System.out.println( "run " + i );
+					final RealTransform transformCopy = transform.copy();
+
+					final CellGrid cellGrid = new CellGrid( outputDimensions, blockSize );
+					final long[] blockGridPosition = new long[ cellGrid.numDimensions() ];
+					cellGrid.getCellGridPositionFlat( i, blockGridPosition );
+
+					final long[] targetMin = new long[ ndims + 1 ];
+					final int[] cellDimensions = new int[ ndims + 1 ];
+					cellGrid.getCellDimensions( blockGridPosition, targetMin, cellDimensions );
+
+					final double[] min = new double[ ndims ];
+					for( int i = 0; i < ndims; i++ )
+					{
+						min[ i ] = targetMin[ i + 1 ];
+					}
+					final double[] minBlock = new double[ ndims ];
+					pixelToPhysical.apply( min, minBlock );
+
+					AffineTransform pixelToPhysicalBlock = new AffineTransform( ndims );
+					pixelToPhysicalBlock.concatenate( pixelToPhysical );
+					pixelToPhysicalBlock.preConcatenate( new Translation( minBlock ));
+
+					ArrayImg< T, ? > blockImg = blockFactory.create( outputDimensions );
+					IntervalView< T > blockPermuted = N5DisplacementField.permute( blockImg, permutation );
+
+					if( isQuantized )
+						trasformToDeformationQuantized( 
+								transformCopy, (RandomAccessibleInterval <? extends IntegerType>) blockPermuted, pixelToPhysicalBlock, maxError );
+					else 
+						trasformToDeformation( transformCopy, 
+								(RandomAccessibleInterval <? extends RealType>) blockPermuted, pixelToPhysicalBlock );
+
+					T type = quantizationType.copy();
+					// write the block
+					try
+					{
+						System.out.println( "save block " + i + " of " + numBlocks );
+						N5Utils.saveNonEmptyBlock( blockImg, n5Writer, dataset, blockGridPosition, type );
+					}
+					catch ( IOException e )
+					{
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+	}
+
+	public static final <T extends RealType<T>> void trasformToDeformation(
+			final RealTransform transform,
+			final RandomAccessibleInterval<T> deformationField )
+	{
+		double[] s = DoubleStream.iterate( 1, x -> x ).limit( transform.numSourceDimensions() ).toArray();
+		trasformToDeformation( transform, deformationField,
+				new Scale( s ));
+	}
+
+	/**
+	 * Stores the given {@link RealTransform} into the given deformation field.
+	 * 
+	 * @param transform the transformation
+	 * @param deformationField the deformation field
+	 * @param pixelToPhysical the transform from discrete coordinates to physical coordinates
+	 */
+	public static final <T extends RealType<T>> void trasformToDeformation(
+			final RealTransform transform,
+			final RandomAccessibleInterval<T> deformationField,
+			final AffineGet pixelToPhysical)
+	{
+		assert deformationField.numDimensions() == ( transform.numSourceDimensions() + 1 );
+		assert deformationField.dimension( deformationField.numDimensions() - 1 ) >= transform.numSourceDimensions();
+
+		final int N = transform.numSourceDimensions();
+		final RealPoint pt = new RealPoint( transform.numTargetDimensions() );
+		final RealPoint ptXfm = new RealPoint( transform.numTargetDimensions() );
+
+		final CompositeIntervalView< T, ? extends GenericComposite< T > > col = Views.collapse( deformationField );
+		final Cursor< ? extends GenericComposite< T > > c = Views.flatIterable( col ).cursor();
+		while ( c.hasNext() )
+		{
+			final GenericComposite< T > displacementVector = c.next();
+
+			// transform the location of the cursor
+			// and store the displacement
+			pixelToPhysical.apply( c, pt );
+			transform.apply( pt, ptXfm );
+
+			for ( int i = 0; i < N; i++ )
+				displacementVector.get( i ).setReal( ptXfm.getDoublePosition( i ) - pt.getDoublePosition( i ) ); 
+		}
+	}
+
+	/**
+	 * Stores the given {@link RealTransform} into the given deformation field.
+	 * 
+	 * @param transform the transformation
+	 * @param deformationField the deformation field
+	 * @param pixelToPhysical the transform from discrete coordinates to physical coordinates
+	 */
+	public static final <Q extends IntegerType<Q>> void trasformToDeformationQuantized(
+			final RealTransform transform,
+			final RandomAccessibleInterval<Q> deformationField,
+			final AffineGet pixelToPhysical,
+			final Converter< DoubleType, Q > quantizer )
+	{
+		assert deformationField.numDimensions() == ( transform.numSourceDimensions() + 1 );
+		assert deformationField.dimension( deformationField.numDimensions() - 1 ) >= transform.numSourceDimensions();
+
+		final int N = transform.numSourceDimensions();
+		final RealPoint pt = new RealPoint( transform.numTargetDimensions() );
+		final RealPoint ptXfm = new RealPoint( transform.numTargetDimensions() );
+
+		final CompositeIntervalView< Q, ? extends GenericComposite< Q > > col = Views.collapse( deformationField );
+		final Cursor< ? extends GenericComposite< Q > > c = Views.flatIterable( col ).cursor();
+
+		DoubleType displacement = new DoubleType();
+		Q value = Util.getTypeFromInterval( deformationField ).copy();
+		while ( c.hasNext() )
+		{
+			final GenericComposite< Q > displacementVector = c.next();
+
+			// transform the location of the cursor
+			// and store the displacement
+			pixelToPhysical.apply( c, pt );
+			transform.apply( pt, ptXfm );
+
+			for ( int i = 0; i < N; i++ )
+			{
+				displacement.setReal( ptXfm.getDoublePosition( i ) - pt.getDoublePosition( i ) );
+				quantizer.convert( displacement, value );
+				displacementVector.get( i ).set( value );
+			}
+		}
+	}
+
+	public static final <Q extends IntegerType<Q>> void trasformToDeformationQuantized(
+			final RealTransform transform,
+			final RandomAccessibleInterval<Q> deformationField,
+			final AffineGet pixelToPhysical,
+			final double maxError )
+	{
+		System.out.println( "xfm 2 dfield quant");
+		trasformToDeformationQuantized( transform, deformationField,
+				pixelToPhysical,
+				quantizationConverter( transform.numSourceDimensions(), maxError, 
+						Util.getTypeFromInterval( deformationField )));
+	}
+
+	public static final <Q extends IntegerType<Q>> void trasformToDeformationQuantized(
+			final RealTransform transform,
+			final RandomAccessibleInterval<Q> deformationField,
+			final double maxError )
+	{
+		double[] s = DoubleStream.iterate( 1, x -> x ).limit( transform.numSourceDimensions() ).toArray();
+		trasformToDeformationQuantized( transform, deformationField,
+				new Scale( s ),
+				quantizationConverter( transform.numSourceDimensions(), maxError, 
+						Util.getTypeFromInterval( deformationField )));
 	}
 
     /**
@@ -357,18 +644,15 @@ public class N5DisplacementField
 
 		// have to bump the dimension up by one to apply it to the displacement field
 		int N = spacing.length;
-		final AffineTransform affineMtx;
+		final AffineGet affineMtx;
 		if ( N == 1 )
-			affineMtx = new AffineTransform( 2 );
+			affineMtx = new Scale( spacing[ 0 ] );
 		else if ( N == 2 )
-			affineMtx = new AffineTransform( 3 );
+			affineMtx = new Scale2D( spacing );
 		else if ( N == 3 )
-			affineMtx = new AffineTransform( 4 );
+			affineMtx = new Scale3D( spacing );
 		else
 			return null;
-
-		for ( int i = 0; i < N; i++ )
-			affineMtx.set( spacing[ i ], i, i );
 
 		return affineMtx;
 	}
@@ -458,7 +742,7 @@ public class N5DisplacementField
      * @param defaultType
      * @return the deformation field as a RealRandomAccessible
      */
-	public static < T extends NativeType< T > & RealType< T > > RealRandomAccessible< T > openCalibratedField(
+	public static < T extends NativeType< T > & RealType< T > >  RealRandomAccessible< T >[] openCalibratedField(
 			final N5Reader n5, final String dataset,
 			final InterpolatorFactory< T, RandomAccessible< T > > interpolator, 
 			final T defaultType ) throws Exception
@@ -483,35 +767,47 @@ public class N5DisplacementField
      * @param defaultType the type
      * @return the deformation field as a RealRandomAccessible
      */
-	public static < T extends NativeType< T > & RealType< T > > RealRandomAccessible< T > openCalibratedField(
+	public static < T extends NativeType< T > & RealType< T > > RealRandomAccessible< T >[] openCalibratedField(
 			final N5Reader n5, final String dataset,
 			final InterpolatorFactory< T, RandomAccessible< T > > interpolator, 
 			final String extensionType,
 			final T defaultType ) throws Exception
 	{
 		RandomAccessibleInterval< T > dfieldRai = openField( n5, dataset, defaultType );
-		RandomAccessibleInterval< T > dfieldRaiPerm = vectorAxisLast( dfieldRai );
-
 		if ( dfieldRai == null )
 		{
 			return null;
 		}
-		RealRandomAccessible< T > dfieldReal = null;
-		if( extensionType.equals( EXTEND_MIRROR )){
-			dfieldReal = Views.interpolate( Views.extendMirrorDouble( dfieldRaiPerm ), interpolator );
-		}
-		else if( extensionType.equals( EXTEND_BORDER )){
-			dfieldReal = Views.interpolate( Views.extendBorder( dfieldRaiPerm ), interpolator );
-		}
-		else {
-			dfieldReal = Views.interpolate( Views.extendZero( dfieldRaiPerm ), interpolator );
+
+		RandomAccessibleInterval< T > dfieldRaiPerm = vectorAxisLast( dfieldRai );
+
+		// num spatial dimensions
+		final int nd = dfieldRaiPerm.numDimensions() - 1;
+		@SuppressWarnings( "unchecked" )
+		RealRandomAccessible<T>[] displacements = new RealRandomAccessible[ nd ];
+
+		for( int i = 0; i < nd; i++ )
+		{
+			IntervalView< T > coordDist = Views.hyperSlice( dfieldRaiPerm, nd, i );
+			RealRandomAccessible< T > dfieldReal = null;
+			if( extensionType.equals( EXTEND_MIRROR )){
+				dfieldReal = Views.interpolate( Views.extendMirrorDouble( coordDist ), interpolator );
+			}
+			else if( extensionType.equals( EXTEND_BORDER )){
+				dfieldReal = Views.interpolate( Views.extendBorder( coordDist ), interpolator );
+			}
+			else {
+				dfieldReal = Views.interpolate( Views.extendZero( coordDist ), interpolator );
+			}
+
+			final AffineGet pix2Phys = openPixelToPhysical( n5, dataset );
+			if ( pix2Phys != null )
+				displacements[ i ] = RealViews.affine( dfieldReal, pix2Phys );
+			else
+				displacements[ i ] = dfieldReal;
 		}
 
-		final AffineGet pix2Phys = openPixelToPhysical( n5, dataset );
-		if ( pix2Phys != null )
-			return RealViews.affine( dfieldReal, pix2Phys );
-
-		return dfieldReal;
+		return displacements;
 	}
 
     /**
